f54f5eb424a947faf8101a603fe70c1a8860d414
balmor <dduda@nexway.com>
2023-01-01 18:36:47 +0000

    Deploying to gh-pages from @ balmor/exchange-rate@a4b08ead327ca646c501c477a0d5cd097f796549 🚀

--- src/components/StyledContainer/StyledContainer.tsx
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-
-export const StyledContainer = styled('div')`
-  width: 70rem;
-  margin: 0 auto;
-`;
--- src/theme/palette.ts
+++
@@ -1,9 +0,0 @@
-export const palette = {
-  white: '#ffffff',
-  dark: '#182331',
-  darkFront: '#1A2635',
-  lightGray: '#424242',
-  yellow: '#fdd835',
-  orange: '#ffa726',
-  black: '#000000',
-};
